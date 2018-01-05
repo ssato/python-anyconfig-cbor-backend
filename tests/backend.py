8169933c3fc1f2ec4457e426d45ff1e302e98fdd
Satoru SATOH <satoru.satoh@gmail.com>
2018-01-06 03:08:27 +0900

    initial checkin

---
+++ tests/backend.py
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2017, 2018 Satoru SATOH <ssato @ redhat.com>
+# License: MIT
+#
+# pylint: disable=missing-docstring,invalid-name,too-few-public-methods
+from __future__ import absolute_import
+
+import anyconfig_cbor_backend.cbor as TT
+import tests.common as TBC
+
+
+class HasParserTrait(TBC.HasParserTrait):
+
+    psr = TT.Parser()
+    cnf = dict(a=0, b="bbb", c=5, sect0=dict(d=["x", "y", "z"]))
+    cnf_s = TT.cbor.dumps(cnf)
+
+
+class Test_10(TBC.Test_10_dumps_and_loads, HasParserTrait):
+
+    load_options = dump_options = dict(sort_keys=False)
+
+
+class Test_20(TBC.Test_20_dump_and_load, HasParserTrait):
+
+    pass
+
+# vim:sw=4:ts=4:et:
