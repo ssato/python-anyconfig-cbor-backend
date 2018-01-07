40fd9668a371157855755700f7b86f59001aff2c
Satoru SATOH <satoru.satoh@gmail.com>
2018-01-07 22:40:09 +0900

    change: remove cbor2 backend to simplify dependencies

--- anyconfig_cbor_backend/__init__.py
+++ anyconfig_cbor_backend/__init__.py
@@ -1,7 +1,6 @@
 """anyconfig backend to support Java properties files.
 """
 from __future__ import absolute_import
-from .cbor import Parser as CBORParser
-from .cbor2 import Parser as CBOR2Parser
+from .cbor import Parser
 
-__all__ = ["CBORParser", "CBOR2Parser"]
+__all__ = ["Parser"]
